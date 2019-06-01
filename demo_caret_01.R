66773b11b650645317791daf73ae1899c79e096f
Rafael Lima <ls.rafael@icloud.com>
2019-06-01 19:31:37 -0300

    setup script

---
+++ demo_caret_01.R
@@ -0,0 +1,16 @@
+# Demonstração de uso do pacote Caret para machine learning no R
+
+# Diretório de trabalho no OS X
+setwd('/Users/ls_rafael/Documents/GitHub/machine_learning_com_caret')
+
+#### 1. Setup Pacotes ####
+install.packages('caret')
+install.packages('randomForest')
+
+#### 2. Load Pacotes ####
+library(caret)
+library(randomForest)
+library(datasets)
+
+#### 3. Dataset mtcars ####
+
