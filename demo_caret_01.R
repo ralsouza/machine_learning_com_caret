afbbd631165afaefc71634d1abaf762509c686d8
Rafael Lima <ls.rafael@icloud.com>
2019-06-01 20:36:20 -0300

    Splitting do dataset.

--- demo_caret_01.R
+++ demo_caret_01.R
@@ -12,5 +12,22 @@ library(caret)
 library(randomForest)
 library(datasets)
 
-#### 3. Dataset mtcars ####
+# Visualização das funções contidas no pacote Caret
+# O pacote Caret é muito completo para machine learning
+names(getModelInfo())
+
+#### 3. Visualização do mtcars ####
+View(mtcars)
+
+#### 4. Splitting do Dataset ####
+?createDataPartition
+index <- createDataPartition(y = mtcars$mpg, p = 0.7, list = FALSE)
+
+ds_treino <- mtcars[ index, ]
+ds_teste  <- mtcars[-index, ]
+
+#### 5. Treinamento do Modelo ####
+# ATENÇÃO: Existe a função train() também no pacote generics
+?caret::train
+
 
