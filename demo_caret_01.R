21c84abe8dcbe6e2ea0e9eba6d71ef0d68b88350
Rafael Lima <ls.rafael@icloud.com>
2019-06-02 00:09:29 -0300

    Demo ok.

--- demo_caret_01.R
+++ demo_caret_01.R
@@ -51,9 +51,13 @@ varImp(modelo_v1)
 # gear   0.7745
 # vs     0.0000
 
+# Plot das Variáveis
+plot(varImp(modelo_v9))
+
 # Conforme a análise de relevância, as variáveis cyl e wt possuem força de correlação de 63% e 100% respectivamente
 
 #### 7. Treinamento dos Modelos com as Variáveis mais Relevantes ####
+# Treinamento com Regressão Linear
 modelo_v1 <- train(mpg ~ wt + cyl, data = ds_treino, method = 'lm')
 modelo_v2 <- train(mpg ~ wt + cyl + disp, data = ds_treino, method = 'lm')
 modelo_v3 <- train(mpg ~ wt + cyl + disp + carb, data = ds_treino, method = 'lm')
@@ -63,6 +67,10 @@ modelo_v6 <- train(mpg ~ wt + cyl + disp + carb + hp + drat + am, data = ds_trei
 modelo_v7 <- train(mpg ~ wt + cyl + disp + carb + hp + drat + am + qsec, data = ds_treino, method = 'lm')
 modelo_v8 <- train(mpg ~ wt + cyl + disp + carb + hp + drat + am + qsec + gear, data = ds_treino, method = 'lm')
 modelo_v9 <- train(mpg ~ wt + cyl + disp + carb + hp + drat + am + qsec + gear + vs, data = ds_treino, method = 'lm')
+
+# Treinamento com Random Forest
+modelo_rf_v1 <- train(mpg ~.,data = ds_treino, method = 'rf')
+
 
 #### 8. Resumo Descritivo dos Resultados de Precisão ####
 summary(modelo_v1) # Coeficiente de Determinação: 84,90%
@@ -75,27 +83,33 @@ summary(modelo_v7) # Coeficiente de Determinação: 87,84%
 summary(modelo_v8) # Coeficiente de Determinação: 87,85%
 summary(modelo_v9) # Coeficiente de Determinação: 87,86%
 
+summary(modelo_rf_v1) # O resumo do Randon Forest não aprensenta nível de precisão, deverá ser calculado
 
+#### 9. Ajustes dos Parâmetros de Treinamento Modelo ####
+# Usando método cv (Cross Validation), para dividir os dados em vários pedaços
+# tanto de treino quanto de testes. Criando 10 folders, ou seja, 10 combibações
+# de treino e teste totalmente randômicos. E usa-se isso como um tipo de controle
+?trainControl
 
+controle1 <- trainControl(method = 'cv', number = 10)
 
+# Recalculando o modelo com o parâmetro de controle
+modelo_v10 <- train( mpg ~.
+                    ,data = ds_treino
+                    ,method = 'lm'
+                    ,trcontrol = controle1
+                    ,metric = 'Rsquared')
 
+summary(modelo_v10)
+# Não houve melhora de precisão com esta técnica, mas em alguns casos poderia ajudar
 
+#### 10. Análise dos Resíduos (Erros das Predições do Modelo) ####
+residuals <- resid(modelo_v10)
+residuals
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#### 11. Execução das Predições ####
+?predict
+predict_values <- predict(modelo_v9, ds_teste)
+predict_values
+plot(ds_teste$mpg, predict_values)
 
